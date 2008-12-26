acd7d2748ef2041b7c3f9f33f67e986a21c90517
fedorchenko <fedorchenko@dad14252-c51f-11dd-9d8b-4f985c3f9f46>
2008-12-26 15:01:24 +0000

    DALC: Linq integration.
    git-svn-id: https://nicnet.googlecode.com/svn/trunk@27 dad14252-c51f-11dd-9d8b-4f985c3f9f46

---
+++ src/NI.Data.Dalc.Linq/DalcData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace NI.Data.Dalc.Linq
+{
+	public class DalcData<TData> : IOrderedQueryable<TData> {
+
+
+		public DalcData(IDalc provider, Expression expression) {
+
+		}
+
+	}
+}
