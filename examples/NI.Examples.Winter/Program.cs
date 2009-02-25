aaf28529e563fda9fad1c182b6747efda34175f8
shigasumi <shigasumi@dad14252-c51f-11dd-9d8b-4f985c3f9f46>
2009-02-25 08:09:53 +0000

    Examples for NI.Winter
    git-svn-id: https://nicnet.googlecode.com/svn/trunk@88 dad14252-c51f-11dd-9d8b-4f985c3f9f46

--- examples/NI.Examples.Winter/Program.cs
+++ examples/NI.Examples.Winter/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Configuration;
 
 using NI.Winter;
+using NI.Common;
 
 namespace NI.Examples.Winter
 {
@@ -12,7 +13,65 @@ namespace NI.Examples.Winter
     {
         static void Main(string[] args)
         {
-            IComponentsConfig config = ConfigurationManager.GetSection("components") as IComponentsConfig;
+            // get 'components' section
+            Console.WriteLine(
+                String.Format(
+                        "===START CREATING CONFIG==="
+                    )
+                );
+            IComponentsConfig config = ConfigurationSettings.GetConfig("components") as IComponentsConfig;
+            Console.WriteLine(
+                String.Format(
+                        "===END CREATING CONFIG==="
+                    )
+                );
+            // or variant that is marked as not deprecated
+            // IComponentsConfig config = ConfigurationManager.GetSection("components") as IComponentsConfig;
+
+            // create named service provider instance
+            Console.WriteLine(
+                String.Format(
+                        "===START CREATING SERVICE PROVIDER==="
+                    )
+                );
+            INamedServiceProvider srv = new ServiceProvider(config);
+            Console.WriteLine(
+                String.Format(
+                        "===END CREATING SERVICE PROVIDER==="
+                    )
+                );
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                String.Format(
+                        "[Service] name: {0}, {1}", "datetimenow", srv.GetService("datetimenow")
+                    )
+                );
+
+            Console.WriteLine(
+                String.Format(
+                        "[Service] name: {0}, {1}", "datetimenow-3days", srv.GetService("datetimenow-3days")
+                    )
+                );
+
+            // create simple object via config
+            TestClass t1 = srv.GetService("nonLazyNonSingletonTestComponent") as TestClass;
+            Console.WriteLine(
+                String.Format(
+                        "[Service] name: {0}, {1}", "nonLazyNonSingletonTestComponent", t1.ToString()
+                    )
+                );
+
+            // create 'lazy' object via config : object will be created only when GetService is called (i.e. by demand)
+            TestClass t2 = srv.GetService("lazyNonSingletonTestComponent") as TestClass;
+            Console.WriteLine(
+                String.Format(
+                        "[Service] name: {0}, {1}", "lazyNonSingletonTestComponent", t2.ToString()
+                    )
+                );
+
+            Console.ReadLine();
         }
     }
 }
