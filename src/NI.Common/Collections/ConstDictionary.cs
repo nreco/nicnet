51dcaa6b597aa395e1b3e8d01d4f87cdaf8bc9f8
fedorchenko <fedorchenko@dad14252-c51f-11dd-9d8b-4f985c3f9f46>
2009-05-25 08:19:25 +0000

    Winter: bugfix related to map entries order
    git-svn-id: https://nicnet.googlecode.com/svn/trunk@120 dad14252-c51f-11dd-9d8b-4f985c3f9f46

--- src/NI.Common/Collections/ConstDictionary.cs
+++ src/NI.Common/Collections/ConstDictionary.cs
@@ -1,17 +1,3 @@
-#region License
-/*
- * Open NIC.NET library (http://nicnet.googlecode.com/)
- * Copyright 2004-2008 NewtonIdeas
- * Distributed under the LGPL licence
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-#endregion
-
 using System;
 using System.Collections;
 using System.Text;
@@ -22,7 +8,9 @@ namespace NI.Common.Collections {
     public class ConstDictionary : IDictionary {
 		object[] ArrKeys;
 		object[] ArrValues;
-		bool isSmall = true;
+		object[] orderedKeys = null;
+		int[] orderedIndexes = null;
+		bool isOrdered = false;
 		bool _IsReadOnly = true;
 		IDictionary UnderlyingDictionary = null;
 		
@@ -31,9 +19,15 @@ namespace NI.Common.Collections {
 			ArrValues = values;
 			if (ArrKeys.Length!=ArrValues.Length)
 				throw new InvalidOperationException("Amount of keys and values should be identical");
-			if (ArrKeys.Length>15) { // good enough suggestion?
-				isSmall = false;
-				Array.Sort(ArrKeys, ArrValues);
+			if (ArrKeys.Length>32) { // good enough suggestion?
+				isOrdered = true;
+				orderedKeys = new object[ArrKeys.Length];
+				orderedIndexes = new int[ArrKeys.Length];
+				for (int i=0; i<ArrKeys.Length; i++) {
+					orderedKeys[i] = ArrKeys[i];
+					orderedIndexes[i] = i;
+				}
+				Array.Sort(orderedKeys, orderedIndexes);
 			}
 		}
 		public ConstDictionary(object[] keys, object[] values, bool isReadOnly) : this(keys,values) {
@@ -43,7 +37,11 @@ namespace NI.Common.Collections {
 		public ConstDictionary(ConstDictionary copyFrom) {
 			ArrKeys = (object[])copyFrom.ArrKeys.Clone();
 			ArrValues = (object[])copyFrom.ArrValues.Clone();
-			isSmall = copyFrom.isSmall;
+			isOrdered = copyFrom.isOrdered;
+			if (isOrdered) {
+				orderedKeys = (object[])copyFrom.orderedKeys.Clone();
+				orderedIndexes = (int[])copyFrom.orderedIndexes.Clone();
+			}
 		}
 		public ConstDictionary(ConstDictionary copyFrom, bool isReadOnly) : this(copyFrom) {
 			_IsReadOnly = isReadOnly;
@@ -75,7 +73,7 @@ namespace NI.Common.Collections {
 		public bool Contains(object key) {
 			if (UnderlyingDictionary!=null)
 				return UnderlyingDictionary.Contains(key);
-			return isSmall ? Array.IndexOf(ArrKeys,key)>=0 : Array.BinarySearch(ArrKeys, key)>=0;
+			return isOrdered ? Array.BinarySearch(orderedKeys, key)>=0 : Array.IndexOf(ArrKeys,key)>=0;
 		}
 
 		public IDictionaryEnumerator GetEnumerator() {
@@ -125,7 +123,8 @@ namespace NI.Common.Collections {
 			get {
 				if (UnderlyingDictionary!=null)
 					return UnderlyingDictionary[key];
-				int idx = isSmall ? Array.IndexOf(ArrKeys, key) : Array.BinarySearch(ArrKeys, key);
+				int idx = isOrdered ? Array.IndexOf(ArrKeys, key) : Array.BinarySearch(orderedKeys, key);
+				if (isOrdered && idx>=0) idx = orderedIndexes[idx];
 				return idx>=0 ? ArrValues[idx] : null;
 			}
 			set {
