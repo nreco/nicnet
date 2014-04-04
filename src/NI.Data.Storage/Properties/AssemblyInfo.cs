05930d2f23fa0b751229959016c67f5eec3d7169
Vitalii Fedorchenko <fedorchenko@gmail.com>
2014-04-04 17:23:50 +0300

    NI.Data.Storage: Finalize basic dynamic storage functionality - nuspec for NI.Data.Storage

--- src/NI.Data.Storage/Properties/AssemblyInfo.cs
+++ src/NI.Data.Storage/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NReco.Storage")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("NI.Data.Storage")]
+[assembly: AssemblyDescription("Virtual business data storage based on underlying DALC (SQL database).")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("NReco.Storage")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCompany("NewtonIdeas")]
+[assembly: AssemblyProduct("NI.Data.Storage")]
+[assembly: AssemblyCopyright("Copyright © NewtonIdeas 2013-2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
